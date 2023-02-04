1c388a3fccbeb7b71ba3b4f0db79b6fb9f0be305
Guthen <114919245+arkaht@users.noreply.github.com>
2023-02-04 01:19:11 +0100

    [wip] obscure area + test scene

---
+++ Assets/ObscureArea.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Game
+{
+	public class ObscureArea : MonoBehaviour
+	{
+		[SerializeField]
+		private float pushForce = 10.0f;
+		[SerializeField]
+		private Vector3 pushDirection = Vector3.forward;
+
+		private new Collider collider;
+
+		void Awake()
+		{
+			collider = GetComponent<Collider>();
+		}
+
+		void OnTriggerStay( Collider collider )
+		{
+			if ( !collider.TryGetComponent( out CharacterController controller ) ) return;
+
+			Vector3 dir = pushDirection;//collider.transform.position - transform.position;
+			float dist_ratio = dir.sqrMagnitude / collider.bounds.size.sqrMagnitude;
+			controller.Move( Time.deltaTime * pushForce * dist_ratio * dir.normalized );
+		}
+
+		void OnValidate()
+		{
+			pushDirection = pushDirection.normalized;
+		}
+
+		void OnDrawGizmos()
+		{
+			if ( !collider ) Awake();
+
+			Gizmos.color = Color.cyan;
+			Gizmos.DrawWireCube( collider.bounds.center, collider.bounds.size );
+
+			Gizmos.DrawLine( transform.position, transform.position + pushDirection );
+		}
+	}
+}
