e606bb72cac08b27a75c1c005774d693a58dcace
Guthen <114919245+arkaht@users.noreply.github.com>
2023-02-05 20:43:38 +0100

    [added] fast rotation helper

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -3,6 +3,16 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
+    [SerializeField]
+    private float fastRotationMultiplier = 2.0f;
+    [SerializeField]
+    private float accelerationTime = 1.0f;
+    [SerializeField]
+    private float accelerationSpeed = 5.0f;
+
+    private float currentAccelerationTime = 0.0f;
+    private float currentSpeedMultiplier = 1.0f;
+
     private Grabber grabber;
     private FirstPersonController controller;
 
@@ -14,11 +24,20 @@ public class Rotator : MonoBehaviour
 
     void Update()
     {
-        if ( !grabber.Grabbable || !grabber.Grabbable.Rotable || !grabber.IsCurrentlyGrabbing ) return;
-
         float rotation = controller.Input.rotation;
-        if ( rotation == 0.0f ) return;
+        if ( !grabber.Grabbable || !grabber.Grabbable.Rotable || !grabber.IsCurrentlyGrabbing || rotation == 0.0f ) 
+        {
+            currentAccelerationTime = 0.0f;
+            currentSpeedMultiplier = 1.0f;
+            return;
+        }
 
-        grabber.Grabbable.Rotable.Rotate( rotation * Time.deltaTime );
+        //  accelerate while holding for long time
+        if ( ( currentAccelerationTime += Time.deltaTime ) >= accelerationTime )
+        {
+            currentSpeedMultiplier = Mathf.Lerp(currentSpeedMultiplier, fastRotationMultiplier, Time.deltaTime * accelerationSpeed);
+        }
+
+        grabber.Grabbable.Rotable.Rotate( rotation * currentSpeedMultiplier * Time.deltaTime );
     }
 }
